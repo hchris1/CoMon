a38840acba2eef85af40fc9742fd0a121133596e
hchris1 <horn_christian@web.de>
2024-01-06 23:27:24 +0100

    Bump version to 0.1.1

--- aspnet-core/src/CoMon.Core/AppVersionHelper.cs
+++ aspnet-core/src/CoMon.Core/AppVersionHelper.cs
@@ -13,7 +13,7 @@ namespace CoMon
         /// Gets current version of the application.
         /// It's also shown in the web page.
         /// </summary>
-        public const string Version = "0.1.0";
+        public const string Version = "0.1.1";
 
         /// <summary>
         /// Gets release (last build) date of the application.
