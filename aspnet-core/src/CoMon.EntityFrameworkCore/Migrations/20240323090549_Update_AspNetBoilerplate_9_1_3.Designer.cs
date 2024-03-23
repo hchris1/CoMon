96a107bdb5e7059da52a6a155525748912950b79
Christian Horn <34690225+hchris1@users.noreply.github.com>
2024-03-23 10:11:28 +0100

    Update dependencies (#82)

--- aspnet-core/src/CoMon.EntityFrameworkCore/Migrations/20240323090549_Update_AspNetBoilerplate_9_1_3.Designer.cs
+++ aspnet-core/src/CoMon.EntityFrameworkCore/Migrations/20240323090549_Update_AspNetBoilerplate_9_1_3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CoMon.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoMon.Migrations
 {
     [DbContext(typeof(CoMonDbContext))]
-    partial class CoMonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240323090549_Update_AspNetBoilerplate_9_1_3")]
+    partial class Update_AspNetBoilerplate_9_1_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
