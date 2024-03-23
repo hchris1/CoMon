97e26c4910f75f83c114a01b5e32763708be606f
hchris1 <horn_christian@web.de>
2024-03-23 10:09:05 +0100

    Update dependencies

---
+++ aspnet-core/src/CoMon.EntityFrameworkCore/Migrations/20240323090549_Update_AspNetBoilerplate_9_1_3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoMon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_AspNetBoilerplate_9_1_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FailReason",
+                table: "AbpUserLoginAttempts",
+                type: "TEXT",
+                maxLength: 1024,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailReason",
+                table: "AbpUserLoginAttempts");
+        }
+    }
+}
